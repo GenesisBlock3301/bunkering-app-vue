716db6548875bc1fe45a0be4d0e21f50334063b0
genesisblock3301 <nur15-1463@diu.edu.bd>
2023-12-13 22:34:48 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import SliderComponent from "@/components/SliderComponent.vue";
+import AboutComponent from "@/components/AboutComponent.vue";
+import ServiceComponent from "@/components/ServiceComponent.vue";
+import FleetComponent from "@/components/FleetComponent.vue";
+import CTaComponent from "@/components/CTaComponent.vue";
+import TeamComponent from "@/components/TeamComponent.vue";
+import FAQComponent from "@/components/FAQComponent.vue";
+import ContactComponent from "@/components/ContactComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import WhyUsComponent from "@/components/WhyUsComponent.vue";
+</script>
+
+<template>
+  <HeaderComponent/>
+  <SliderComponent/>
+  <ServiceComponent/>
+  <FleetComponent/>
+  <AboutComponent/>
+  <CTaComponent/>
+  <WhyUsComponent/>
+  <TeamComponent/>
+  <FAQComponent/>
+  <ContactComponent/>
+  <FooterComponent/>
+</template>
+
---
+++ src/components/FleetComponent.vue
@@ -0,0 +1,96 @@
+<script setup>
+import {ref} from "vue";
+let id = 0
+const fleetImages = ref([
+  {
+    id: id++,
+    name: "Hawks Javaahiru",
+    img1: "/src/assets/img/Javaahiru-1.webp",
+    img2: "/src/assets/img/Javaahiru-2.webp",
+    features:[
+      {
+        name: "Year built",
+        amount: 2000
+      },
+      {
+        name: "Cargo Capacity",
+        amount: "5.5 Million liters"
+      }
+    ]
+  },
+  {
+    id: id++,
+    name: "Hawks Javaahiru 2",
+    img1: "/src/assets/img/Javaahiru-1.webp",
+    img2: "/src/assets/img/Javaahiru-2.webp",
+    features:[
+      {
+        name: "Year built",
+        amount: 2000
+      },
+      {
+        name: "Cargo Capacity2",
+        amount: "5.5 Million liters2"
+      }
+    ]
+  },
+])
+let isHover = ref({});
+
+for (let i = 1; i <= fleetImages.length; i++) {
+  isHover.value[i] = false;
+}
+function onMouseEnter(index){
+  console.log("mouse enter", isHover.value[index])
+  isHover.value[index] = true
+}
+function onMouseLeave(index){
+  console.log("mouse leave", isHover.value[index])
+  isHover.value[index] = false
+}
+
+</script>
+
+<template>
+  <div id="services" class="services section-bg">
+    <div class="section-title">
+      <h2>Fleet</h2>
+    </div>
+    <div class="row">
+      <div class="flex h-full flex-col">
+        <div class="group" v-for="image in fleetImages" :key="image.id">
+          <div class="h-96">
+            <p class="flex justify-center text-xl font-bold transition-all duration-700 ease-in-out group-hover:text-[#EFB00F]">
+            {{ image.name }}
+            </p>
+            <div class="">
+              <div @mouseover="onMouseEnter(image.id)" @mouseleave="onMouseLeave(image.id)" class="flex justify-center">
+                <!--black-->
+                <img  :src="image.img1"
+                     class="absolute transition-all duration-500 opacity-100 ease-in-out"
+                      :class="{'opacity-100': isHover[image.id], 'opacity-0': !isHover[id]}"
+                />
+                <img  :src="image.img2"
+                     class="absolute transition-all duration-500 opacity-0 ease-in-out"
+                      :class="{'opacity-100': !isHover[image.id], 'opacity-0': isHover[image.id]}"
+                />
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="grid translate-y-32 grid-cols-3 justify-center gap-10 gap-y-4 transition-all duration-700 ease-in-out sm:flex sm:gap-10 md:translate-y-24 md:gap-20 md:opacity-0 md:group-hover:translate-y-36 md:group-hover:opacity-100">
+                <div v-for="item in image.features" :key="item.id" class="flex flex-col gap-y-2">
+                  <h1 class="text-center text-xs text-[#707070]">{{ item.name }}</h1>
+                  <p class="text-center text-xs">{{ item.amount }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
